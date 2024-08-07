f92bd5bd3d9c09b71d22ff7c21dc23e008847f9f
shodeq <alfiananwarshodiqi@gmail.com>
2024-08-07 11:08:56 +0700

    package: • react-icons

--- src/components/fragments/Navbar.jsx
+++ src/components/fragments/Navbar.jsx
@@ -1,7 +1,9 @@
+import { FaBell } from "react-icons/fa";
+
 export default function Navbar () {
     return(
         <div className="w-full flex justify-center items-center border-2 border-blue-500">
-            <h1>Navbar</h1>
+            <h1>Navbar <FaBell /></h1>
         </div>
     )
 }
